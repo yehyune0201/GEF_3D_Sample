ee541d00162ac391a27fe1f3bbef6b071176f529
yehyune0201 <yehyune0201@gmail.com>
2026-09-17 12:14:55 +0900

    [Feat]복셀 캐릭터와 아레나

--- GFE A YEEEEEE/Assets/Script/Player Conttoller.cs
+++ GFE A YEEEEEE/Assets/Script/Player Conttoller.cs
@@ -4,21 +4,37 @@ using UnityEngine.InputSystem;
 public class PlayerConttoller : MonoBehaviour
 {
 
-    public float movespeed = 5f;
+    public float moveSpeed = 5f;
     public float jumpPower = 5f;
     public float gravity = -20f;
+    public float mouseSensitivity = 0.2f;
+    private Vector2 lookInput;
+    private bool isRunning;
+    public Transform cameraPivot;
+    public Transform cameraTransform;
+
+    private float pitch = 20f;
+
 
     private float verticalVelocity;
     private Vector2 moveInput;
     private CharacterController controller;
+
+
     
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
     }
-    
+
     void Update()
     {
+        transform.Rotate(0f, lookInput.x * mouseSensitivity, 0f);
+        pitch = pitch - lookInput.y * mouseSensitivity;
+        pitch = Mathf.Clamp(pitch, -20f, 60f);
+        cameraPivot.localEulerAngles = new Vector3(pitch, 0f, 0f);
+
         if (controller.isGrounded && verticalVelocity < 0f)
         {
             verticalVelocity = -2f;
@@ -26,19 +42,28 @@ public class PlayerConttoller : MonoBehaviour
 
         verticalVelocity += gravity * Time.deltaTime;
 
-        Vector3 move = new Vector3(moveInput.x, 0,moveInput.y);
-        move = move * movespeed;
+        Vector3 move = transform.forward * moveInput.y + transform.right * moveInput.x;
+
+        float speed = moveSpeed;
+        float targetZ = -3f;
+        if (isRunning)
+        {
+            speed = moveSpeed * 2f;
+            targetZ = -5f;
+        }
+        move = move * speed;
         move.y = verticalVelocity;
 
+        Vector3 camPos = cameraTransform.localPosition;
+        camPos.z = Mathf.Lerp(camPos.z, targetZ, 5f * Time.deltaTime);
+        cameraTransform.localPosition = camPos;
 
-        controller.Move(move* movespeed * Time.deltaTime);
-       
+        controller.Move(move* moveSpeed * Time.deltaTime);
     }
     public void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
     }
-
     public void OnJump(InputValue value)
     {
         if (value.isPressed && controller.isGrounded)
@@ -46,4 +71,13 @@ public class PlayerConttoller : MonoBehaviour
             verticalVelocity = jumpPower;
         }
     }
+    public void OnLook(InputValue value)
+    {
+        lookInput = value.Get<Vector2>();
+    }
+    public void OnSprint(InputValue value)
+    {
+        isRunning = value.isPressed;
+    }
+
 }
